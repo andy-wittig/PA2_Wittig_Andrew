2870f48815175b7def862fd531dcb6ab48409733
Andy Wittig <andywittig10@gmail.com>
2025-09-29 18:05:46 -0700

    readFile func implemented

--- threaded_sum.c
+++ threaded_sum.c
@@ -15,6 +15,10 @@
 #include <sys/time.h>
 //-----End Includes-----
 
+//-----Func Definitions-----
+int readFile(char filePath[], int array[]);
+//-----End Definitions-----
+
 int main(int argc, char* argv[])
 {
   if (argc < 3 || argc > 3) //Not the correct number of cl args
@@ -22,5 +26,43 @@ int main(int argc, char* argv[])
     printf ("The incorrect number of arguments were provided, expected 3 but %d were given.\n", argc);
     return -1;
   }
+  
+  //Read File and return the number of values parsed
+  char* fPath = argv[1];
+  int arraySize = 100000000;
+  int* numArray = (int*)malloc(arraySize * sizeof(int));
+  
+  int numParsed = readFile(fPath, numArray);
+
+  int threadsReq = atoi(argv[2]);
+  if (threadsReq > numParsed)
+  {
+    printf("Too many threads requested!\n");
+    free(numArray);
+    return -1;
+  }
+
+  free(numArray);
   return 0;
+}
+
+int readFile(char filePath[], int array[])
+{
+  FILE* filePointer;
+  filePointer = fopen(filePath, "r");
+
+  if (filePointer == NULL)
+  {
+    printf("The file could not be opened!\n");
+    return -1;
+  }
+
+  int valueRead, valuesParsed = 0;
+  while (fscanf(filePointer, "%d", &valueRead) == 1)
+  {
+    array[valuesParsed++] = valueRead;
+  }
+  
+  fclose(filePointer);
+  return valuesParsed;
 }
